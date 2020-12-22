3c7133a09140a01bc1a9f65a794f420911180af9
Dzhem35 <64707331+Dzhem35@users.noreply.github.com>
2020-12-22 19:35:59 +0300

    Add files via upload

---
+++ hw-1.py
@@ -0,0 +1,14 @@
+
+x = input("1. Number :")
+y = input("2. Number :")
+z = input("3. Number :")
+a = input("4. Number :")
+b = input("5. Number :")
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+
+print("First:" + str(x) + " Second:" + str(y) + " Third:" + str(z) +" Fourth:" + str(a) +" Fifth:"+ str(b))
